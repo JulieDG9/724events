41b2a015473a10045a88f1e77f4580250e6bc872
prosg <learner.in.dev@gmail.com>
2024-09-18 13:48:43 +0200

    Added required fields and email type - Form

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -5,9 +5,16 @@ import "./style.scss";
 export const FIELD_TYPES = {
   INPUT_TEXT: 1,
   TEXTAREA: 2,
+  EMAIL: 3,
 };
 
-const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
+const Field = ({
+  type = FIELD_TYPES.INPUT_TEXT,
+  label,
+  name,
+  placeholder,
+  required,
+}) => {
   let component;
   switch (type) {
     case FIELD_TYPES.INPUT_TEXT:
@@ -15,19 +22,34 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
         <input
           type="text"
           name={name}
+          required={required}
           placeholder={placeholder}
           data-testid="field-testid"
         />
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" />;
+      component = (
+        <textarea name={name} required={required} data-testid="field-testid" />
+      );
+      break;
+    case FIELD_TYPES.EMAIL:
+      component = (
+        <input
+          type="email"
+          name={name}
+          required={required}
+          placeholder={placeholder}
+          data-testud="field-testid"
+        />
+      );
       break;
     default:
       component = (
         <input
           type="text"
           name={name}
+          required={required}
           placeholder={placeholder}
           data-testid="field-testid"
         />
@@ -46,12 +68,14 @@ Field.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
+  required: PropTypes.bool,
 };
- Field.defaultProps = {
-   label: "",
-   placeholder: "",
-   type: FIELD_TYPES.INPUT_TEXT,
-   name: "field-name",
- }
+Field.defaultProps = {
+  label: "",
+  placeholder: "",
+  type: FIELD_TYPES.INPUT_TEXT,
+  name: "field-name",
+  required: false,
+};
 
 export default Field;
--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -33,8 +33,8 @@ const Form = ({ onSuccess, onError }) => {
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+          <Field placeholder="" label="Nom" required />
+          <Field placeholder="" label="Prénom" required />
           <Select
             selection={["Personel", "Entreprise"]}
             onChange={() => null}
@@ -42,7 +42,13 @@ const Form = ({ onSuccess, onError }) => {
             type="large"
             titleEmpty
           />
-          <Field placeholder="" label="Email" name="email" />
+          <Field
+            placeholder=""
+            label="Email"
+            name="email"
+            type={FIELD_TYPES.EMAIL}
+            required
+          />
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
           </Button>
@@ -52,6 +58,7 @@ const Form = ({ onSuccess, onError }) => {
             placeholder="message"
             label="Message"
             type={FIELD_TYPES.TEXTAREA}
+            required
           />
         </div>
       </div>
